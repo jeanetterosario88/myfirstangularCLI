a1fd09ba526345531a310deb0f62927ebf0705f1
jeanetterosario88 <jeanetterosario88@gmail.com>
2021-06-21 20:54:54 -0400

    added directives in servers component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,12 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = "No server was created!";
+  serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() { 
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
+    //button becomes clickable after 2000 milliseconds
   }
+
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = "Server was created. Name is" + this.serverName;  
+  }
+
+  onUpdateServerName(event:any){
+    this.serverName = (<HTMLInputElement>event.target).value;
+ //We have to tell TypeScript explicitly that the target of the event is an input element, by writing ...
+//(<HTMLInputElement>event.target).value or (event.target as HTMLInputElement).value.
+//Otherwise TypeScript will complain, since other HTMLElements don't have a value property, and therefore TypeScript doesn't know if event.target.value is valid syntax.
+}
 
   ngOnInit(): void {
   }
